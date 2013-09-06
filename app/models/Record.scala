7d9bd4234ecf93d53a58b199107bc4615ee0f53e
Karthik Chandraraj <ckarthik17@gmail.com>
2013-09-06 20:09:28 +0530

    CK : pair chart completed

--- app/models/Record.scala
+++ app/models/Record.scala
@@ -1,9 +1,11 @@
 package models
 
+import play.api.db.slick._
 import play.api.db.slick.Config.driver.simple._
 import java.util.Date
 import slick.lifted.MappedTypeMapper
 import play.api.Play
+import play.api.Play.current
 
 case class Record(id: Option[Long] = None, date: Date, dev1: String, dev2: String, task: String)
 
@@ -21,7 +23,23 @@ object Records extends Table[Record]("RECORD") {
 
   def * = id.? ~ date ~  dev1 ~ dev2 ~ task <> (Record.apply _, Record.unapply _)
   
-  def getDevs = {
+  def pairCount(dev: String) = {
+    DB.withSession { implicit session:Session =>
+      val records = Query(Records).where(r => (r.dev1 === dev || r.dev2 === dev)).list
+      val pairs = for(record <- records) yield (record.dev1, record.dev2)
+      val pairedWithList = pairs.map { p =>        
+        if(p._1 == dev) { 
+          if(p._2 == "") dev else p._2
+        } 
+        else p._1
+      } 
+      val pairedWithCount = pairedWithList.groupBy(identity).mapValues(_.size).withDefaultValue(0)
+      val result = for(d <- devs) yield pairedWithCount(d)
+      result.mkString("['" + dev + "',",",","]")
+    }    
+  }
+
+  def devs = {
     Play.current.configuration.getString("devs").get.split(",")
   }
 }
