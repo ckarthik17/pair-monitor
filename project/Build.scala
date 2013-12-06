e82e119cd409918b3967e37cea990aa20c95424e
Rajesh Pitty <rajesh.pitty@crest-premedia.in>
2013-12-06 20:06:25 +0530

    upgrade play to 2.2.0

--- project/Build.scala
+++ project/Build.scala
@@ -7,11 +7,12 @@ object ApplicationBuild extends Build {
   val appName         = "pair_monitor"
   val appVersion      = "1.0-SNAPSHOT"
 
-  val appDependencies = Seq(    
+  val appDependencies = Seq( 
+    "com.typesafe.play" %% "play-slick" % "0.5.0.8"   
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(    
   	scalaVersion := "2.10.2"
-  ).dependsOn(RootProject(file("/Users/ckarthik/code/play-slick")))
+  )
 
 }
